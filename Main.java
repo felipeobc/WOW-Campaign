30832912daab36a2cf7891520abea984a4febea7
felipeobc <felipe.c@outlook.com>
2019-05-04 13:13:03 -0300

    teste

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    int start;
+    System.out.println("Seja bem-vindo a uma pequena experiência em uma fantastica narrativa dentro no universo de Warcraft (A Queda de Ventobravo)");
+    System.out.println("(1) - Começar (2) - Sair");
+    System.out.print("Escolha uma Opção: ");
+    start = ler.nextInt();
+
+    if(start == 1){
+      //Chamar uma função
+    }else{
+      System.out.print("Obrigado por jogar\nFelipe Oliveira\nGuilherme Correia");
+    }
+    
+
+  }
+
+}
